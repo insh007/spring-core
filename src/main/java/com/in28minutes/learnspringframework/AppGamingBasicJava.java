e4de9e9c3fed8730eaa0828f5af9d89127165705
insh007 <aliinshad007@gmail.com>
2023-10-12 13:40:15 +0530

    till tight coupling and Loose coupling using interface

---
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+import com.in28minutes.learnspringframework.game.GameRunner;
+
+// Example of Tight Coupling
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		
+//		var game = new MarioGame();
+//		var game = new SuperContraGame();
+		var game = new PacmanGame();  // Object creation
+		
+		var gameRunner = new GameRunner(game);
+		// Object creation + Wiring of Dependencies
+		// Game is a Dependency of GameRunner 
+		gameRunner.run();
+	}
+
+}
---
+++ src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learnspringframework.game;
+
+public class PacmanGame implements GamingConsole {
+	public void up() {
+		System.out.println("Go Up eat");
+	}
+	
+	public void down() {
+		System.out.println("Go down eat");
+	}
+	
+	public void left() {
+		System.out.println("Go back eat");
+	}
+	
+	public void right() {
+		System.out.println("Go right eat");
+	}
+	
+}
