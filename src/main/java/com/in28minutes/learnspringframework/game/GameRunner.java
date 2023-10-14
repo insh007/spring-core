1f1d04dacbea9ae6560d91d28a1a3bc903457c65
insh007 <aliinshad007@gmail.com>
2023-10-14 13:45:45 +0530

    give preference using @Primary and @Qualifier

--- src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,10 +10,17 @@ public class GameRunner {
 	
 	private GamingConsole game;
 	
-	public GameRunner(GamingConsole game) {
+	// @Primary Game class methods will run 
+//	public GameRunner(GamingConsole game) {
+//		this.game = game;
+//	}
+	
+	// use @Qualifier
+	// Now SuperContraGame method will run using @Qualifier
+	public GameRunner(@Qualifier ("SuperContraGameQualifier") GamingConsole game) {
 		this.game = game;
 	}
-
+	
 	public void run() {
 		System.out.println("Running game: "+game);
 		game.up();
