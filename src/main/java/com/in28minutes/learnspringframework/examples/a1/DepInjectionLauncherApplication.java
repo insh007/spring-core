8b5bfff0055e52691dc74dfd11cb3fe65a51fa57
insh007 <aliinshad007@gmail.com>
2023-10-14 19:17:45 +0530

    dependency injection using setters and constructor

--- src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -8,19 +8,39 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+// Note : Three ways to Autowiring are : 
+//		using Field reflection, using Setters, using Constructor
+
 @Component
 class YourBusinessClass {
-	@Autowired // using Filed reflection
 	Dependency1 dependency1;
-	
-	@Autowired  // using Filed reflection
 	Dependency2 dependency2;
 	
+	@Autowired
+	// @Autowired annotation is not mandatory in case of Constructor Dependency Injection	
+	public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+		super();
+		System.out.println("Constructor Injection - YourBusinessClass");
+		this.dependency1 = dependency1;
+		this.dependency2 = dependency2;
+	}
+
+//	@Autowired  // using setter
+//	public void setDependency1(Dependency1 dependency1) {
+//		System.out.println("Setter Injection - setDependency1");
+//		this.dependency1 = dependency1;
+//	}
+//
+//	@Autowired  // using setter
+//	public void setDependency2(Dependency2 dependency2) {
+//		System.out.println("Setter Injection - setDependency2");
+//		this.dependency2 = dependency2;
+//	}
+
 	@Override
 	public String toString() {
 		return "YourBusinessClass [dependency1=" + dependency1 + ", dependency2=" + dependency2 + "]";
 	}
-	
 	
 }
 
